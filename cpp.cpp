04c4b3d60122dbddce93a07d1de128ea7a496ecc
vancesec <94236103+vancesec@users.noreply.github.com>
2024-03-20 01:43:29 -0400

    Add files via upload

---
+++ cpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ using namespace std;
+
+ int main()
+{
+	int a;
+	int b;
+	int sum;
+
+	cout << "enter the number hoss /n";
+	cin >> a;
+
+	cout << "enter the another numer /n";
+	cin >> b;
+
+	sum = a + b;
+	cout << "the sum of those no. is " << sum << endl;
+return 0;
+}
